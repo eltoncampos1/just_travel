ea439f30ba11836e6e09577038dfc610ce05c7fa
Elton Campos <eltoncampos36@gmail.com>
2023-09-22 19:28:39 -0300

    chore: setup actions o server

---
+++ .credo.exs
@@ -0,0 +1,21 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/", "src/", "web/", "apps/"],
+        excluded: []
+      },
+      plugins: [],
+      requires: [],
+      strict: false,
+      parse_timeout: 5000,
+      color: true,
+      checks: %{
+        enabled: [
+          {Credo.Check.Design.AliasUsage, priority: :low}
+        ]
+      }
+    }
+  ]
+}
--- mix.exs
+++ mix.exs
@@ -51,7 +51,10 @@ defmodule JustTravel.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:money, "~> 1.12"}
+      {:money, "~> 1.12"},
+
+      # Code analisys
+       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
