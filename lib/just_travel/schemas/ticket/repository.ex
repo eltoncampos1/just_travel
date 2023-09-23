6cd1404d322950376b1e2b9fc1a3019ccbeb3b6a
Elton Campos <eltoncampos36@gmail.com>
2023-09-23 12:27:47 -0300

    feat: Repository for tickets

---
+++ lib/just_travel/schemas/ticket/repository.ex
@@ -0,0 +1,17 @@
+defmodule JustTravel.Schemas.Ticket.Repository do
+  alias JustTravel.Repo
+  alias JustTravel.Schemas
+
+  import Ecto.Query
+
+  def list_tickets_by_location_name(location_name) do
+    from(tkt in Schemas.Ticket, as: :ticket)
+    |> join(:left, [ticket: tkt], loc in Schemas.Location,
+      as: :location,
+      on: loc.id == tkt.location_id
+    )
+    |> Schemas.Location.Query.by_location_name(location_name)
+    |> select([ticket: tkt], tkt)
+    |> Repo.all()
+  end
+end
