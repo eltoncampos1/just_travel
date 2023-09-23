bc457a56ccbb96ccc9a5d4c005d10459dca8e846
Elton Campos <eltoncampos36@gmail.com>
2023-09-23 12:41:49 -0300

    chore: add typespecs

--- lib/just_travel/schemas/location/query.ex
+++ lib/just_travel/schemas/location/query.ex
@@ -4,6 +4,8 @@ defmodule JustTravel.Schemas.Location.Query do
   """
   import Ecto.Query
 
+  @spec by_location_name(queryable :: Ecto.Queryable.t(), location_name :: String.t()) ::
+          Ecto.Queryable.t()
   def by_location_name(queryable, location_name) when bit_size(location_name) > 0 do
     pattern = "%#{location_name}%"
     where(queryable, [location: location], ilike(location.name, ^pattern))
--- lib/just_travel/schemas/ticket/repository.ex
+++ lib/just_travel/schemas/ticket/repository.ex
@@ -4,6 +4,8 @@ defmodule JustTravel.Schemas.Ticket.Repository do
 
   import Ecto.Query
 
+  @spec list_tickets_by_location_name(location_name :: String.t()) ::
+          list(Schemas.Ticket.t()) | []
   def list_tickets_by_location_name(location_name) do
     from(tkt in Schemas.Ticket, as: :ticket)
     |> join(:left, [ticket: tkt], loc in Schemas.Location,
