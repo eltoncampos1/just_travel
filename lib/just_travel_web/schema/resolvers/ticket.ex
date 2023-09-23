67aadd73fca304a63a385eb496e732b98002671a
Elton Campos <eltoncampos36@gmail.com>
2023-09-23 14:18:45 -0300

    refactor: Ticket resolver

--- lib/just_travel_web/schema/resolvers/ticket.ex
+++ lib/just_travel_web/schema/resolvers/ticket.ex
@@ -1,6 +1,13 @@
 defmodule JustTravelWeb.Schema.Resolvers.Ticket do
-  alias JustTravel.Ticket
+  alias JustTravel.Ticket.Commands
+  alias JustTravel.Ticket.Services
+  alias JustTravel.Utils
 
-  def list(%{filters: filters}, _ctx),
-    do: Ticket.Services.ListByLocationName.execute(filters.location_name)
+  def list(%{filters: filters}, _ctx) do
+    with {:ok, %Commands.ListByFilters{} = list_command} <-
+           Utils.Changesets.cast_and_apply(Commands.ListByFilters, filters),
+         tickets when is_list(tickets) <- Services.ListByFilters.execute(list_command) do
+      {:ok, tickets}
+    end
+  end
 end
