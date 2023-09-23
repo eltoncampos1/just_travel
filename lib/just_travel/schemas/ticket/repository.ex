32bf2984885bedaaa147d2ff0079361f0846222b
Elton Campos <eltoncampos36@gmail.com>
2023-09-23 14:16:56 -0300

    feat: add functions to get tickets by add and by many filters

--- lib/just_travel/schemas/ticket/repository.ex
+++ lib/just_travel/schemas/ticket/repository.ex
@@ -1,4 +1,5 @@
 defmodule JustTravel.Schemas.Ticket.Repository do
+  alias JustTravel.Schemas.Ticket
   alias JustTravel.Repo
   alias JustTravel.Schemas
 
@@ -16,4 +17,42 @@ defmodule JustTravel.Schemas.Ticket.Repository do
     |> select([ticket: tkt], tkt)
     |> Repo.all()
   end
+
+  def by_ticket_id(ticket_id) do
+    from(tkt in Schemas.Ticket, as: :ticket)
+    |> Schemas.Ticket.Query.by_ticket_id(ticket_id)
+    |> Repo.one()
+    |> case do
+      nil -> {:error, :not_found}
+      %Schemas.Ticket{} = ticket -> {:ok, ticket}
+    end
+  end
+
+  def list do
+    Repo.all(Ticket)
+  end
+
+  def list(filters) when is_map(filters) do
+    from(tkt in Schemas.Ticket, as: :ticket)
+    |> join_location()
+    |> apply_filters(filters)
+    |> Repo.all()
+  end
+
+  defp join_location(queryable) do
+    queryable
+    |> join(:left, [ticket: tkt], loc in Schemas.Location,
+      as: :location,
+      on: loc.id == tkt.location_id
+    )
+  end
+
+  defp apply_filters(queryable, filters) when is_map(filters) do
+    Enum.reduce(filters, queryable, &filter/2)
+  end
+
+  defp filter({:location_name, location_name}, queryable),
+    do: Schemas.Location.Query.by_location_name(queryable, location_name)
+
+  defp filter({:id, id}, queryable), do: Schemas.Ticket.Query.by_ticket_id(queryable, id)
 end
