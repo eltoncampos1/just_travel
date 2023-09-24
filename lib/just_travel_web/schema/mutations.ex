f93dcbf5e113b985d16d43c842d373a9dad688ac
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 11:17:44 -0300

    feat: mutation to create cart

---
+++ lib/just_travel_web/schema/mutations.ex
@@ -0,0 +1,6 @@
+defmodule JustTravelWeb.Schema.Mutations do
+  use Absinthe.Schema.Notation
+  alias JustTravelWeb.Schema.Mutations
+
+  import_types Mutations.Cart
+end
---
+++ lib/just_travel_web/schema/mutations/cart.ex
@@ -0,0 +1,14 @@
+defmodule JustTravelWeb.Schema.Mutations.Cart do
+  use Absinthe.Schema.Notation
+  alias JustTravelWeb.Schema.Resolvers.Cart, as: CartResolver
+
+  import AbsintheErrorPayload.Payload
+
+  object :cart_mutations do
+    @desc "Create cart mutation"
+    field :create_cart, type: :cart_payload do
+       resolve &CartResolver.create/2
+       middleware &build_payload/2
+    end
+  end
+end
