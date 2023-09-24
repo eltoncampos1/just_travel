4bc6bf1b2ed4c73c69ba8b819dda6ff81d013593
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 11:16:58 -0300

    refactor: cart repository

--- lib/just_travel/schemas/cart/repository.ex
+++ lib/just_travel/schemas/cart/repository.ex
@@ -7,13 +7,46 @@ defmodule JustTravel.Schemas.Cart.Repository do
   @spec new(cart_id :: binary()) :: JustTravel.Schemas.Cart.t()
   def new(cart_id) do
     cart = %Cart{id: cart_id}
-    :ets.insert(@name, {cart_id, cart})
+    upsert(cart_id, cart)
+    {:ok, cart}
+  end
+
+  defp upsert(cart_id, cart), do: :ets.insert(@name, {cart_id, cart})
+
+  def add_item(cart_id, item) do
+    case find_cart(cart_id) do
+      {:ok, cart} ->
+        cart = add(cart, item)
+        upsert(cart_id, cart)
+        {:ok, cart}
+
+      {:error, :not_found} ->
+        {:error, :not_found}
+    end
   end
 
   def find_cart(cart_id) do
     case :ets.lookup(@name, cart_id) do
-      [] -> {:error, []}
+      [] -> {:error, :not_found}
       [{_cart_id, value}] -> {:ok, value}
+    end
+  end
+
+  defp add(%Cart{} = cart, item) do
+    new_price = Money.add(cart.total_price, item.price)
+
+    %Cart{
+      cart
+      | items: add_or_update_qty(cart.items, item),
+        total_qty: cart.total_qty + 1,
+        total_price: new_price
+    }
+  end
+
+  defp add_or_update_qty(items, item) do
+    case Enum.find_index(items, &(&1.item.id == item.id)) do
+      nil -> [%{item: item, qty: 1}] ++ items
+      index -> List.replace_at(items, index, &%{&1 | qty: &1.qty + 1})
     end
   end
 end
