07b4f16144112e8199c69ac095a379e1eae21f73
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 19:09:13 -0300

    test: add ticket to cart command

--- test/just_travel/ticket/commands/add_to_cart_test.exs
+++ test/just_travel/ticket/commands/add_to_cart_test.exs
@@ -22,28 +22,18 @@ defmodule JustTravel.Ticket.Commands.AddToCartTest do
 
   describe "changeset/2" do
     test "returns a valid changeset", %{params: params} do
-      assert "" = Ticket.Commands.AddToCart.changeset(params)
+      assert %Ecto.Changeset{valid?: true} = Ticket.Commands.AddToCart.changeset(params)
     end
 
-    # test "returns error on invalid input" do
-    #   assert %Ecto.Changeset{
-    #            valid?: false,
-    #            errors: [
-    #              cart_id: {"is invalid", [type: :binary_id, validation: :cast]},
-    #              item: {"is invalid", [type: :map, validation: :cast]}
-    #            ]
-    #          } = Cart.Commands.AddItem.changeset(%{cart_id: 123, item: "invalid"})
-    # end
-
-    # for field <- [:cart_id, :item] do
-    #   test "returns error on missing `#{field}`", %{params: params} do
-    #     assert %Ecto.Changeset{
-    #              valid?: false
-    #            } =
-    #              params
-    #              |> Map.drop([unquote(field)])
-    #              |> Cart.Commands.AddItem.changeset()
-    #   end
-    # end
+    for field <- [:id, :price, :description, :date, :category, :location, :country] do
+      test "returns error on missing `#{field}`", %{params: params} do
+        assert %Ecto.Changeset{
+                 valid?: false
+               } =
+                 params
+                 |> Map.drop([unquote(field)])
+                 |> Ticket.Commands.AddToCart.changeset()
+      end
+    end
   end
 end
