ef7ee66fedc82656f7c6a41e6dd2a37adbceaa37
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 11:18:39 -0300

    chore: change names

--- lib/just_travel_web/schema/types/root.ex
+++ lib/just_travel_web/schema/types/root.ex
@@ -3,12 +3,20 @@ defmodule JustTravelWeb.Schema.Types.Root do
 
   alias JustTravelWeb.Schema.Types
   alias JustTravelWeb.Schema.Queries
+  alias JustTravelWeb.Schema.Mutations
 
   import_types Types.Location
   import_types Types.Ticket
-  import_types Queries.Ticket
+  import_types Types.Cart
 
-  object :root_query do
-    import_fields :tickets
+  import_types Queries.Ticket
+  import_types Mutations.Cart
+
+  object :root_queries do
+    import_fields :tickets_queries
+  end
+
+  object :root_mutations do
+    import_fields :cart_mutations
   end
 end
