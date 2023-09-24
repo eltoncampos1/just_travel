e3a4b1f25a7bc4568dcf11eb3a38d078f23d7b63
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 11:45:25 -0300

    feat: get cart by id service

---
+++ lib/just_travel/cart/services/get_cart_by_id.ex
@@ -0,0 +1,7 @@
+defmodule JustTravel.Cart.Services.GetCartById do
+  alias JustTravel.Cart.Command
+  alias JustTravel.Schemas.Cart
+
+  @spec execute(Command.GetCart.t()) :: {:ok, Cart.t()} | {:error, :not_found}
+  def execute(%Command.GetCartById{} = get_cart), do: Cart.Repository.find_cart(get_cart.cart_id)
+end
