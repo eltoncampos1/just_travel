8d64c06352f2187d984074ff6c05053564261e68
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 11:45:10 -0300

    feat: get cart by id command

---
+++ lib/just_travel/cart/command/get_cart_by_id.ex
@@ -0,0 +1,14 @@
+defmodule JustTravel.Cart.Command.GetCartById do
+  use JustTravel.EmbeddedSchema
+
+  @required [:cart_id]
+  embedded_schema do
+    field :cart_id, :binary_id
+  end
+
+  def changeset(schema \\ %__MODULE__{}, params) do
+    schema
+    |> cast(params, @required)
+    |> validate_required(@required)
+  end
+end
