eadab762b71e67fb612b72e743f1025d5d152156
Elton Campos <eltoncampos36@gmail.com>
2023-09-25 01:03:18 -0300

    feat: mutation and schema to delete or decrease item

--- lib/just_travel_web/schema/mutations/ticket.ex
+++ lib/just_travel_web/schema/mutations/ticket.ex
@@ -5,11 +5,20 @@ defmodule JustTravelWeb.Schema.Mutations.Ticket do
   import AbsintheErrorPayload.Payload
 
   object :ticket_mutations do
-    @desc "Create ticket cart mutation"
     field :add_ticket_to_cart, type: :ticket_cart_payload do
+      @desc "Create ticket cart mutation"
       arg :cart_id, non_null(:id)
       arg :ticket_id, non_null(:id)
       resolve &TicketResolver.add_to_cart/2
+      middleware &build_payload/2
+    end
+
+    field :remove_ticket_from_cart, type: :ticket_cart_payload do
+      @desc "Decrease ticket cart mutation"
+      arg :cart_id, non_null(:id)
+      arg :item_id, non_null(:id)
+      arg :action, :actions
+      resolve &TicketResolver.remove/2
       middleware &build_payload/2
     end
   end
