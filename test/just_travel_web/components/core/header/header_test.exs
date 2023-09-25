4d7fcdf59f5ffef4de2898ea0c3527d128df951d
Elton Campos <eltoncampos36@gmail.com>
2023-09-25 19:26:52 -0300

    chore: lint

--- test/just_travel_web/components/core/header/header_test.exs
+++ test/just_travel_web/components/core/header/header_test.exs
@@ -2,14 +2,13 @@ defmodule JustTravelWeb.Core.HeaderTest do
   use JustTravelWeb.ConnCase
   import Phoenix.LiveViewTest
 
-  test "header",%{conn: conn} do
+  test "header", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
 
     header = "[data-role=HEADER]"
 
-    assert  has_element?(view, header)
-    assert element(view, header<>">div>p>", "Cotação dólar hoje: R$5,53")
-    assert element(view, header<>">div>div>div>button>span", "Entrar")
-
+    assert has_element?(view, header)
+    assert element(view, header <> ">div>p>", "Cotação dólar hoje: R$5,53")
+    assert element(view, header <> ">div>div>div>button>span", "Entrar")
   end
 end
