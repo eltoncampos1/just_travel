6793b78f9bd28a4b36efeeef3b3f18c5070934ef
Elton Campos <eltoncampos36@gmail.com>
2023-09-24 21:21:58 -0300

    feat: add pagination and test

---
+++ test/just_travel/utils/repo_test.exs
@@ -0,0 +1,25 @@
+defmodule JustTravel.Utils.RepoTest do
+  use JustTravel.DataCase, async: true
+  alias JustTravel.Schemas.Cart.Repository
+  alias JustTravel.Schemas.Ticket.Repository
+
+  describe "paginate/2" do
+    test "should paginate" do
+      loc = insert(:location)
+      per_page = 4
+      insert_list(100,:ticket, location: loc)
+
+     assert length(Repository.list) == 100
+     assert length(Repository.list(%{paginate: %{page: 1, per_page: per_page}})) == per_page
+    end
+
+    test "should paginate if no page was sent" do
+      loc = insert(:location)
+      per_page = 4
+      insert_list(100,:ticket, location: loc)
+
+     assert length(Repository.list) == 100
+     assert length(Repository.list(%{paginate: %{per_page: per_page}})) == per_page
+    end
+  end
+end
