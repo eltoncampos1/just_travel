b963aa214d097c50390a378e865d4156f110acad
Elton Campos <eltoncampos36@gmail.com>
2023-09-25 19:26:34 -0300

    test: render header component

---
+++ test/just_travel_web/components/core/header/header_test.exs
@@ -0,0 +1,15 @@
+defmodule JustTravelWeb.Core.HeaderTest do
+  use JustTravelWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  test "header",%{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    header = "[data-role=HEADER]"
+
+    assert  has_element?(view, header)
+    assert element(view, header<>">div>p>", "Cotação dólar hoje: R$5,53")
+    assert element(view, header<>">div>div>div>button>span", "Entrar")
+
+  end
+end
