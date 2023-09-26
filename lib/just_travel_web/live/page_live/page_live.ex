93747eab175c312176fdf4cd2ac9082ada725783
Elton Campos <eltoncampos36@gmail.com>
2023-09-26 20:13:56 -0300

    feat: handle pagination

--- lib/just_travel_web/live/page_live/page_live.ex
+++ lib/just_travel_web/live/page_live/page_live.ex
@@ -1,28 +1,69 @@
 defmodule JustTravelWeb.PageLive do
   use JustTravelWeb, :live_view
 
-  alias JustTravelWeb.PageLive.Components
   alias JustTravel.Schemas.Ticket.Repository
+  alias JustTravelWeb.PageLive.Components
+  alias JustTravelWeb.Core
 
-  def mount(_params, _session, socket) do
-    tickets = Repository.list()
+  def handle_params(params, _uri, socket) do
+    page = String.to_integer(params["page"] || "1")
+    per_page = String.to_integer(params["per_page"] || "4")
+    location = Map.get(socket.assigns, :location, "")
+    options = %{page: page, per_page: per_page}
 
-    socket =
-      socket
-      |> assign(:location, "")
-      |> assign(:tickets, tickets)
+    {tickets, total} = get_tickets(location, options)
 
-    {:ok, socket}
+    max_pages = div(total, per_page) |> round()
+
+    assigns = [
+      options: options,
+      tickets: tickets,
+      total: total,
+      location: location,
+      max_pages: max_pages
+    ]
+
+    socket = assign(socket, assigns)
+
+    {:noreply, socket}
   end
 
   def handle_event("filter_by_location", %{"location" => location}, socket) do
-    tickets = Repository.list(%{location_name: location})
+    options = socket.assigns.options
+
+    {tickets, total} = get_tickets(location, options)
+
+    assigns = [location: location, tickets: tickets, total: total]
+    IO.inspect(socket.assigns)
+    socket =
+      socket
+      |> assign(:assigns, socket.assigns)
+      |> assign(assigns)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("select_page", %{"page" => page}, socket) do
+    page = String.to_integer(page)
+    options = socket.assigns.options |> Map.put(:page, page)
+
+    {tickets, total} = get_tickets(socket.assigns.location, options)
+    assigns = [tickets: tickets, total: total]
 
     socket =
       socket
-      |> assign(:location, location)
-      |> assign(:tickets, tickets)
+      |> assign(:assigns, socket.assigns)
+      |> assign(options: options)
+      |> assign(assigns)
+      |> push_patch(to: "/?page=#{page}&per_page=#{options.per_page}")
 
     {:noreply, socket}
+  end
+
+  defp get_tickets(location, options) do
+    {:ok, tickets: tickets, total: total} =
+      Repository.list(%{location_name: location, paginate: options})
+
+    {tickets, total}
   end
 end
