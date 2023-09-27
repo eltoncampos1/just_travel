3192bc21c4eed99a8b01049a23a50df954de322b
Elton Campos <eltoncampos36@gmail.com>
2023-09-27 20:37:42 -0300

    fix: server

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,7 +16,9 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
-config :just_travel, JustTravelWeb.Endpoint, server: true
+if System.get_env("PHX_SERVER") do
+  config :just_travel, JustTravelWeb.Endpoint, server: true
+end
 
 if config_env() == :prod do
   database_url =
@@ -46,19 +48,18 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  port = System.get_env("PORT") || 4000
-  host = System.get_env("PHX_HOST") || "localhost"
-  app = System.get_env("APP_NAME") || "just-travel"
+  app = System.get_env("APP_NAME")
 
   config :just_travel, JustTravelWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    server: true,
+    url: [host: app <> ".gigalixirapp.com", port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port,
+      port: {:system, "PORT"},
       check_origin: [
         "https://just-travel.gigalixirapp.com/",
         "wss://just-travel.gigalixirapp.com/",
@@ -69,7 +70,7 @@ if config_env() == :prod do
         "//.#{app}.gigalixir.com:4000"
       ],
       https: [
-        port: port,
+        port: {:system, "PORT"},
         check_origin: [
           "https://just-travel.gigalixirapp.com/",
           "wss://just-travel.gigalixirapp.com/",
