4ce685bbc14ab369125ebb6ec14ccf0321f6acfc
Elton Campos <eltoncampos36@gmail.com>
2023-09-27 16:11:52 -0300

    fix: test

--- test/just_travel_web/components/core/paginate/paginate_test.exs
+++ test/just_travel_web/components/core/paginate/paginate_test.exs
@@ -3,6 +3,11 @@ defmodule JustTravelWeb.Core.PaginateTest do
   import Phoenix.LiveViewTest
 
   test "paginate", %{conn: conn} do
+    loc = insert(:location, name: "TOKYO", country: "JAPAN")
+    for _i <- 1..5 do
+      ticket = insert(:ticket, location: loc)
+      insert(:ticket_price, ticket: ticket)
+    end
     {:ok, view, _html} = live(conn, "/")
 
     select = "[data-role=SELECT_PAGE]"
